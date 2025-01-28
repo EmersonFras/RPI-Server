83453393dec63727d67c71fce8468f6eea16a083
emersonfras <emersonfrasure@gmail.com>
2025-01-27 21:20:32 -0500

    Adds route to start album C program

--- src/routes/albumRoutes.js
+++ src/routes/albumRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const axios = require('axios')
 
 router.get('/', async (req, res) => {
+
     const accessToken = req.user.access_token
 
     try {
@@ -18,6 +19,26 @@ router.get('/', async (req, res) => {
         res.json(response.data) 
     } catch (error) {
         res.status(500).send('Error occurred while fetching data from Spotify API')
+    }
+})
+
+router.post('/display', (req, res) => {
+    if (!scriptProcess) {
+        const { img } = req.body
+
+        scriptProcess = spawn('sudo', ['../AlbumArt/album_art', img], {
+            stdio: 'inherit',  // Inherit the stdio to see output in your server console
+        })
+
+        // Reset the scriptProcess variable when the script exits
+        scriptProcess.on('close', (code) => {
+            console.log(`Child process exited with code ${code}`)
+            scriptProcess = null  // Reset on exit
+        })
+
+        res.send('Script started')
+    } else {
+        res.send('Script is already running')
     }
 })
 
